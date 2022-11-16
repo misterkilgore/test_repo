e0b16ff4abc648b58452257b5febced2a8b79f91
MisterKilgore <mcantoni81@gmail.com>
2022-11-16 22:32:12 +0100

    02-Correct setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='toolbox',
+    name='biggmodules',
     version='0.0.3',
     author='Michele Cantoni',
     author_email='mcantoni81@gmail.com',
@@ -13,9 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/misterkilgore/test_repo',
     project_urls = {
-        "Bug Tracker": "https://github.com/mike-huls/toolbox/issues"
+        "Bug Tracker": "https://github.com/misterkilgore/test_repo/issues"
     },
     license='MIT',
-    packages=['toolbox'],
-    install_requires=['requests'],
+    packages=['biggmodules'],
+    install_requires=['numpy'],
 )
